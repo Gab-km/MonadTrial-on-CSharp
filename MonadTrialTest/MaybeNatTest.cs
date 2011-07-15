cb49b9949404f8b941dfb0258903e11f383244d2
Gab_km <the25thcromosome@gmail.com>
2011-07-16 00:30:47 +0900

    expectedがなくても良さそうだったので、テストから削除。

--- MonadTrialTest/MaybeNatTest.cs
+++ MonadTrialTest/MaybeNatTest.cs
@@ -12,19 +12,17 @@ namespace MonadTrialTest
         [Test]
         public void tryAddの引数に1と2を与えたらJust3が返ってくること()
         {
-            var expected = new Just<int>(3);
             var result = MaybeNatSample.tryAdd(1, 2);
             Assert.That(result.GetType(), Is.EqualTo(typeof(Just<int>)));
-            Assert.That(result.Value, Is.EqualTo(expected.Value));
+            Assert.That(result.Value, Is.EqualTo(3));
         }
 
         [Test]
         public void tryAddの引数に3と4を与えたらJust7が返ってくること()
         {
-            var expected = new Just<int>(7);
             var result = MaybeNatSample.tryAdd(3, 4);
             Assert.That(result.GetType(), Is.EqualTo(typeof(Just<int>)));
-            Assert.That(result.Value, Is.EqualTo(expected.Value));
+            Assert.That(result.Value, Is.EqualTo(7));
         }
 
         [Test]
@@ -44,10 +42,9 @@ namespace MonadTrialTest
         [Test]
         public void doMaybeNestの引数に1と2を与えたらJust3が返ってくること()
         {
-            var expected = new Just<int>(3);
             var result = MaybeNatSample.doMaybeNest(1, 2);
             Assert.That(result.GetType(), Is.EqualTo(typeof(Just<int>)));
-            Assert.That(result.Value, Is.EqualTo(expected.Value));
+            Assert.That(result.Value, Is.EqualTo(3));
         }
 
         [Test]
