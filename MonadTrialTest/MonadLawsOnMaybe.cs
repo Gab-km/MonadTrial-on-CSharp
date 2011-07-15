51076dd41ec0c8fd0a6def25ae6c788c4d23cfda
Gab_km <the25thcromosome@gmail.com>
2011-07-16 00:51:03 +0900

    モナド則その2を確認。

--- MonadTrialTest/MonadLawsOnMaybe.cs
+++ MonadTrialTest/MonadLawsOnMaybe.cs
@@ -10,7 +10,7 @@ namespace MonadTrialTest
     public class MonadLawsOnMaybe
     {
         [Test]
-        public void MaybeIntがMonad即その1を満たすこと()
+        public void MaybeIntがMonad則その1を満たすこと()
         {
             // return x >>= f === f x
             Func<int, Maybe<int>> f = (x) =>
@@ -26,6 +26,28 @@ namespace MonadTrialTest
                 };
             var lefthand = Maybe<int>.Bind(Maybe<int>.Return(1), f);
             var righthand = f(1);
+            Assert.That(lefthand.GetType(), Is.EqualTo(righthand.GetType()));
+            Assert.That(lefthand.Value, Is.EqualTo(righthand.Value));
+        }
+
+        [Test]
+        public void MaybeIntがMonad則その2を満たすこと()
+        {
+            // m >>= return === m
+            Func<int, Maybe<int>> f = (x) =>
+                {
+                    if (x < 0)
+                    {
+                        return new Nothing<int>();
+                    }
+                    else
+                    {
+                        return new Just<int>(x * 2);
+                    }
+                };
+            var m = f(1);
+            var lefthand = Maybe<int>.Bind(m, Maybe<int>.Return);
+            var righthand = m;
             Assert.That(lefthand.GetType(), Is.EqualTo(righthand.GetType()));
             Assert.That(lefthand.Value, Is.EqualTo(righthand.Value));
         }
