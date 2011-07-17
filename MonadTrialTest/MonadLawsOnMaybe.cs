f398b848d7870ba65c1ebf0bff955ac73b9c9ced
Gab_km <the25thcromosome@gmail.com>
2011-07-18 08:50:16 +0900

    Nothing<int>でモナド則その3の確認。

--- MonadTrialTest/MonadLawsOnMaybe.cs
+++ MonadTrialTest/MonadLawsOnMaybe.cs
@@ -139,5 +139,49 @@ namespace MonadTrialTest
             Assert.That(lefthand.GetType(), Is.EqualTo(righthand.GetType()));
             Assert.That(lefthand.Value, Is.EqualTo(righthand.Value));
         }
+
+        [Test]
+        public void MaybeInt_NothingがMonad則その3を満たすこと()
+        {
+            // m >>= (\x -> f x >>= g) === (m >>= f) >>= g
+            Func<int, Maybe<int>> f = (x) =>
+            {
+                if (x < 0)
+                {
+                    return new Nothing<int>();
+                }
+                else
+                {
+                    return new Just<int>(x * 2);
+                }
+            };
+            Func<int, Maybe<int>> g = (x) =>
+            {
+                if (x < 0)
+                {
+                    return new Nothing<int>();
+                }
+                else
+                {
+                    return new Just<int>(x * 3);
+                }
+            };
+            Func<int, Maybe<int>> h = (x) =>
+            {
+                if (x < 0)
+                {
+                    return new Nothing<int>();
+                }
+                else
+                {
+                    return new Just<int>(x);
+                }
+            };
+            var m = h(-1);
+            var lefthand = Maybe<int>.Bind(m, (x) => Maybe<int>.Bind(f(x), g));
+            var righthand = Maybe<int>.Bind(Maybe<int>.Bind(m, f), g);
+            Assert.That(lefthand.GetType(), Is.EqualTo(righthand.GetType()));
+            Assert.That(lefthand.Value, Is.EqualTo(righthand.Value));
+        }
     }
 }
