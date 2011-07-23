19f576dfa84e5e0cb6f386abc17d576d53998904
Gab_km <the25thcromosome@gmail.com>
2011-07-23 13:31:12 +0900

    Maybe<string>のモナド則その2が成立することを確認。

--- MonadTrialTest/MonadLawsOnMaybeString.cs
+++ MonadTrialTest/MonadLawsOnMaybeString.cs
@@ -10,7 +10,7 @@ namespace MonadTrialTest
     public class MonadLawsOnMaybeString
     {
         [Test]
-        public void MaybeStringtがMonad則その1を満たすこと()
+        public void MaybeStringがMonad則その1を満たすこと()
         {
             // return x >>= f === f x
             Func<string, Maybe<string>> f = (x) =>
@@ -27,6 +27,28 @@ namespace MonadTrialTest
             var value = "A";
             var lefthand = Maybe<string>.Bind(Maybe<string>.Return(value), f);
             var righthand = f(value);
+            Assert.That(lefthand.GetType(), Is.EqualTo(righthand.GetType()));
+            Assert.That(Maybe<string>.FromJust(lefthand), Is.EqualTo(Maybe<string>.FromJust(righthand)));
+        }
+
+        [Test]
+        public void MaybeStringがMonad則その2を満たすこと()
+        {
+            // m >>= return === m
+            Func<string, Maybe<string>> f = (x) =>
+            {
+                if (string.IsNullOrEmpty(x))
+                {
+                    return new Nothing<string>();
+                }
+                else
+                {
+                    return new Just<string>(x + x.ToLower());
+                }
+            };
+            var m = f("A");
+            var lefthand = Maybe<string>.Bind(m, Maybe<string>.Return);
+            var righthand = m;
             Assert.That(lefthand.GetType(), Is.EqualTo(righthand.GetType()));
             Assert.That(Maybe<string>.FromJust(lefthand), Is.EqualTo(Maybe<string>.FromJust(righthand)));
         }
