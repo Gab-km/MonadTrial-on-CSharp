5fac4d9571684c264467b0e95e4f8ec15b8ee75c
Gab_km <the25thcromosome@gmail.com>
2011-07-23 13:33:25 +0900

    Maybe<string>がモナド則その3についても成り立つことを確認。

--- MonadTrialTest/MonadLawsOnMaybeString.cs
+++ MonadTrialTest/MonadLawsOnMaybeString.cs
@@ -52,5 +52,49 @@ namespace MonadTrialTest
             Assert.That(lefthand.GetType(), Is.EqualTo(righthand.GetType()));
             Assert.That(Maybe<string>.FromJust(lefthand), Is.EqualTo(Maybe<string>.FromJust(righthand)));
         }
+
+        [Test]
+        public void MaybeStringがMonad則その3を満たすこと()
+        {
+            // m >>= (\x -> f x >>= g) === (m >>= f) >>= g
+            Func<string, Maybe<string>> f = (x) =>
+            {
+                if (string.IsNullOrEmpty(x))
+                {
+                    return new Nothing<string>();
+                }
+                else
+                {
+                    return new Just<string>(x + x.ToLower());
+                }
+            };
+            Func<string, Maybe<string>> g = (x) =>
+            {
+                if (string.IsNullOrEmpty(x))
+                {
+                    return new Nothing<string>();
+                }
+                else
+                {
+                    return new Just<string>(x + x);
+                }
+            };
+            Func<string, Maybe<string>> h = (x) =>
+            {
+                if (string.IsNullOrEmpty(x))
+                {
+                    return new Nothing<string>();
+                }
+                else
+                {
+                    return new Just<string>(x);
+                }
+            };
+            var m = h("A");
+            var lefthand = Maybe<string>.Bind(m, (x) => Maybe<string>.Bind(f(x), g));
+            var righthand = Maybe<string>.Bind(Maybe<string>.Bind(m, f), g);
+            Assert.That(lefthand.GetType(), Is.EqualTo(righthand.GetType()));
+            Assert.That(Maybe<string>.FromJust(lefthand), Is.EqualTo(Maybe<string>.FromJust(righthand)));
+        }
     }
 }
