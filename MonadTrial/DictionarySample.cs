7322c4cf9c18a8696200798359c47a61f5cc4bc7
Gab_km <the25thcromosome@gmail.com>
2011-07-24 09:27:23 +0900

    Dictionary.tryFindの仮実装。

---
+++ MonadTrial/DictionarySample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonadTrial
+{
+    public class DictionarySample
+    {
+        public static Maybe<T> tryFind<T>(string p, Dictionary<string, T> dict)
+        {
+            var value = dict[p];
+            return new Just<T>(value);
+        }
+    }
+}
---
+++ MonadTrialTest/DictionaryTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using MonadTrial;
+
+namespace MonadTrialTest
+{
+    public class DictionaryTest
+    {
+        [Test]
+        public void tryFindの引数にキーとそのキーを持つDictionaryを与えたらJustValueが返ってくること()
+        {
+            var dict = new Dictionary<string, int>();
+            dict.Add("x", 1);
+            dict.Add("y", 2);
+            dict.Add("z", 3);
+            var result = DictionarySample.tryFind("x", dict);
+            Assert.That(result.GetType(), Is.EqualTo(typeof(Just<int>)));
+            Assert.That(Maybe<int>.FromJust(result), Is.EqualTo(1));
+        }
+    }
+}
