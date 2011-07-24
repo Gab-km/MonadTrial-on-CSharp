175d9ae5241bdc31a9f6b7b665cce366d4733d20
Gab_km <the25thcromosome@gmail.com>
2011-07-24 09:33:37 +0900

    Dictionaryの初期化が重複していたのでSetUpに上げるリファクタリング。

--- MonadTrialTest/DictionaryTest.cs
+++ MonadTrialTest/DictionaryTest.cs
@@ -9,13 +9,20 @@ namespace MonadTrialTest
 {
     public class DictionaryTest
     {
-        [Test]
-        public void tryFindの引数にxとxをキーを持つDictionaryを与えたらJust1が返ってくること()
+        Dictionary<string, int> dict;
+
+        [SetUp]
+        public void setUp()
         {
-            var dict = new Dictionary<string, int>();
+            dict = new Dictionary<string, int>();
             dict.Add("x", 1);
             dict.Add("y", 2);
             dict.Add("z", 3);
+        }
+
+        [Test]
+        public void tryFindの引数にxとxをキーを持つDictionaryを与えたらJust1が返ってくること()
+        {
             var result = DictionarySample.tryFind("x", dict);
             Assert.That(result.GetType(), Is.EqualTo(typeof(Just<int>)));
             Assert.That(Maybe<int>.FromJust(result), Is.EqualTo(1));
@@ -24,13 +31,13 @@ namespace MonadTrialTest
         [Test]
         public void tryFindの引数にyとyをキーに持つDictionaryを与えたらJust2が返ってくること()
         {
-            var dict = new Dictionary<string, int>();
-            dict.Add("x", 2);
-            dict.Add("y", 2);
-            dict.Add("z", 3);
             var result = DictionarySample.tryFind("y", dict);
             Assert.That(result.GetType(), Is.EqualTo(typeof(Just<int>)));
             Assert.That(Maybe<int>.FromJust(result), Is.EqualTo(2));
         }
+
+        //[Test]
+        //public void tryFindの引数にwとwをキーに持たないDictionaryを与えたらNothingが返ってくること(){
+
     }
 }
